7a805c75e435ec82d253cab15df18f62dfdd9d6f
qqyoungqq <qianyang@mail.usf.edu>
2015-07-01 14:28:10 -0400

    debug gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 var gulp = require('gulp'),
 	minifyhtml = require('gulp-minify-html');
 
-gulp.task('content',function(){
+gulp.task('default',function(){
 	gulp.src('src/index.html')
 					.pipe(minifyhtml())
-					.pipe(gulp.dest('build/index.html'));
+					.pipe(gulp.dest('build/'));
 });
-
-gulp.task('default','content');
